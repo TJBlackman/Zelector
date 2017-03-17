1bdc72ebe1bd54d56ec933e0062e81c7255e48d4
TJBlackman <trevorblackman@THE-DESTROYER-27.local>
2017-03-16 21:29:27 -0700

    Add response
    Added a response to the extension window that shows the user how many
    dom elements their selector matched. Also, commented every line!

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,17 @@
+(function(){
 
-    var input = document.getElementById('selectorInput');
+    const input = document.getElementById('selectorInput');
+    const result = document.querySelector('.resultNum');
     input.addEventListener('keyup', checkSelectors);
 
     function checkSelectors(e){
-        var selectorValue = input.value;
+        if (input.value.length < 1 ) { result.innerText = '0';}
+
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {selector: selectorValue}, function(response) {
-              // some response could go here.
+          chrome.tabs.sendMessage(tabs[0].id, {selector: input.value}, function(response) {
+              result.innerText = response.found;
           });
         });
     }
+
+}());
--- js/select-them.js
+++ js/select-them.js
@@ -1,44 +1,57 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    getSelectors(request.selector);
-});
+(function(){
 
-function getSelectors(str){
-    // delete all previous ones
-    Array.from(document.querySelectorAll('.selectorHighlight'))
-         .forEach(function(el){ el.remove(); });
-
-
-    if (typeof str !== 'string'){
-        try { str = String(str) }
-        catch(error) { console.log(error); return; }
-    }
-
-    var elements = document.querySelectorAll(str);
-    if ( elements.length < 1 ) {
-        // show message about no valid selections
-        console.log('No selections found.');
-        return;
-    }
-
-    elements.forEach(function(el){
-        var offTop = el.offsetTop;
-        var offLeft = el.offsetLeft;
-        var screenPosition = el.getBoundingClientRect();
-
-        var hl = document.createElement('div');
-            hl.classList.add('selectorHighlight');
-            hl.style.backgroundColor = 'rgba(164, 0, 0, 0.3)';
-            hl.style.border = '1px solid rgba(164, 0, 0, 1)';
-            hl.style.height = (el.offsetHeight - 2)+'px'; // add room for border
-            hl.style.width = (el.offsetWidth - 2)+'px';
-            hl.style.display = 'block';
-            hl.style.zIndex = 999;
-            hl.style.position = 'absolute';
-            hl.style.top = (screenPosition.top + window.scrollY) + 'px';
-            hl.style.left = (screenPosition.left + window.scrollX) + 'px';
-
-        document.body.prepend(hl);
+    // TODO: change this to a data connection so I can listen for extension window close
+    // for now, just clear divs on window click
+    window.addEventListener('click', function(){
+        document.querySelectorAll('.selectorHighlight').forEach(el => el.remove());
     });
 
-}
+    // listen for message from extension window!!
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+        getSelectors(request.selector, sendResponse);
+    });
+
+    function getSelectors(selector, sendResponse){
+        // delete all previous highlights
+        document.querySelectorAll('.selectorHighlight').forEach(el => el.remove());
+
+        // verify selector type
+        if (typeof selector !== 'string'){
+            try { selector = String(selector) }
+            catch(error) { return; }
+        }
+
+        // store dom variable in element
+        let elements;
+        try { elements = document.querySelectorAll(selector); }
+        catch(error) { return; }
+
+
+        // send back data on matched elements
+        sendResponse({"found":elements.length})
+
+        // if nothing found, now is a good time to stop!
+        if ( elements.length < 1 ) { return; }
+
+        // highlight every matched element with a
+        // hovering, absolutely positioned, colored <div>
+        elements.forEach(function(el){
+            const offTop = el.offsetTop;
+            const offLeft = el.offsetLeft;
+            const screenPosition = el.getBoundingClientRect(); // gives coords of matched el
+
+            const hl = document.createElement('div');
+            hl.classList.add('selectorHighlight');
+
+            const height = screenPosition.height + 'px';
+            const width = screenPosition.width + 'px';
+            const top = (screenPosition.top + window.scrollY) + 'px';
+            const left = (screenPosition.left + window.scrollX) + 'px';
+
+            hl.style.cssText = 'background-color: rgba(164, 0, 0, 0.3); border: 1px solid rgba(164, 0, 0, 1); display: block; z-index: 999; position: absolute; top: '+top+'; left: '+left+'; height: '+height+'; width: '+width+';'
+
+            document.body.prepend(hl);
+        });
+    }
+
+}());
